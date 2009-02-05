0128d5c915f965d45224299197b025e2c5b7b00f
Liu Kang-min <gugod@gugod.org>
2009-02-05 08:35:40 +0800

    Implement the core logic

--- lib/App/Lazyd.pm
+++ lib/App/Lazyd.pm
@@ -2,7 +2,32 @@ package App::Lazyd;
 
 use strict;
 use warnings;
+
 our $VERSION = '0.01';
+
+use Net::Delicious;
+use URI::Title qw( title );
+
+sub run {
+    my ($self, $config) = @_;
+
+    die "Need to set both username and password."
+	unless defined($config->{username}) && defined($config->{password});
+    
+    my $del = Net::Delicious->new({
+	user => $config->{username},
+	pswd => $config->{password}
+    });
+
+    my $title = title($url) || "";
+    my $auto_tags = join " ", split /\W/, "$url  $title";
+
+    $del->add_post({
+	url => $url,
+	tags => "$auto_tags" . ($tags||""),
+	description => $url,
+    });
+}
 
 1;
 __END__
